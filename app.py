8ad8254c30d2f1c6f5f45510985f34203d045fe5
code3954 <namju978@gmail.com>
2025-02-05 11:04:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,30 @@ from ui.ml import run_ml
 
 
 def main():
-    st.title('자동차 가격 예측 앱')
+        st.markdown("""
+        <style>
+            .title {
+                position: absolute;
+                top: 10px;
+                left: 10px;
+                font-size: 30px;
+                font-weight: bold;
+            }
+        </style>
+    """, unsafe_allow_html=True)
 
-    menu = ['Home', 'EDA', 'ML']
-    choice = st.sidebar.selectbox('메뉴', menu )
+    # Title displayed at top left corner
+        st.markdown('<div class="title">자동차 가격 예측 앱</div>', unsafe_allow_html=True)
 
-    if choice == menu[0] :
-        run_home()
-    elif choice == menu[1]:
-        run_eda()    
-    elif choice == menu[2]:
-        run_ml()        
+        menu = ['Home', 'EDA', 'ML']
+        choice = st.sidebar.selectbox('메뉴', menu )
+
+        if choice == menu[0] :
+            run_home()
+        elif choice == menu[1]:
+            run_eda()    
+        elif choice == menu[2]:
+            run_ml()        
 
 
 
